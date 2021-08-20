47edbe280b36027b8a29e85344ae43831a6803e4
Pumudu-railway <87109326+Pumudu-railway@users.noreply.github.com>
2021-08-20 22:35:53 +0530

    Create musicplayer.py

---
+++ tg_bot/modules/musicplayer.py
@@ -0,0 +1,27 @@
+__mod_name__ = "Music Player"
+
+__help__ = """
+=>> *Song Playing* 🎧 
+❍ /play <song name> - play song you requested
+❍ /dplay <song name> - play song you requested via deezer
+❍ /splay <song name> - play song you requested via jio saavn
+❍ /playlist - Show now playing list
+❍ /current - Show now playing
+❍ /song <song name> - download songs you want quickly
+❍ /search <query> - search videos on youtube with details
+❍ /deezer <song name> - download songs you want quickly via deezer
+❍ /saavn <song name> - download songs you want quickly via saavn
+❍ /video <song name> - download videos you want quickly
+=>> *Admins only*
+❍ /player - open music player settings panel
+❍ /pause - pause song play
+❍ /resume - resume song play
+❍ /skip - play next song
+❍ /end - stop music play
+❍ /userbotjoin - invite assistant to your chat
+❍ /admincache - Refresh admin list
+=>>   *Use*
+1) Add @Kumikosetsuko in your group to admin with all permission
+2) Add Assistant I'd to your group @TheKumikosetsuko
+3)But currently I can't play music Because,still I am in maintain mood 
+"""
